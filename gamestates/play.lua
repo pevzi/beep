5f55f7bd51886620c532e8b2db675c091a5a5b5d
pevzi <pevzi23@gmail.com>
2015-10-17 19:27:44 +0700

    MorseReader, MultiReader

--- gamestates/play.lua
+++ gamestates/play.lua
@@ -1,7 +1,6 @@
 local r = require "resources"
 local u = require "useful"
 local input = require "input"
-local morse = require "morse"
 local Chat = require "chat"
 local Scenario = require "scenario"
 
@@ -10,9 +9,6 @@ local h = love.graphics.getHeight()
 
 local chatMargin = w / 15
 local lineHeight = h / 15
-
-local dahThreshold = 0.2
-local letterThreshold = 0.2
 
 local beep = r.sources.beep:clone()
 beep:setLooping(true)
@@ -24,75 +20,22 @@ local Play = {}
 function Play:enteredState()
     love.graphics.setBackgroundColor(r.colors.background)
 
-    self.lastPressed = 0
-    self.lastReleased = 0
-    self.duration = 0
-
-    self.code = ""
-    self.buffer = ""
-
-    if not self.chat then
-        self.chat = Chat(chatMargin, chatMargin, w - chatMargin * 2, h - chatMargin * 2 - lineHeight)
-        self.chat:say("Ожидаю код Морзе.", r.colors.speaker2, "right")
-    end
-
+    self.chat = Chat(chatMargin, chatMargin, w - chatMargin * 2, h - chatMargin * 2 - lineHeight)
     self.scenario = Scenario(self.chat)
 end
 
 function Play:update(dt)
     if input.beep:pressed() then
         beep:play()
-        self.duration = 0
-
     elseif input.beep:released() then
         beep:stop()
-        self.code = self.code .. (self.duration > dahThreshold and "-" or ".")
-        self.duration = 0
-    end
-
-    self.duration = self.duration + dt
-
-    if not input.beep:isDown() and self.duration > letterThreshold and self.code ~= "" then
-        local letter = morse[self.code]
-
-        if letter then
-            self.buffer = self.buffer .. letter
-            self.chat:say(("Прочитана буква \"%s\"."):format(letter), r.colors.speaker2, "right")
-        else
-            self.chat:say("Неизвестная последовательность сигналов.", r.colors.speaker2, "right")
-        end
-
-        self.code = ""
-    end
-
-    if input.clear:pressed() then
-        self.chat:say(("Очищен буфер: \"%s\"."):format(self.buffer), r.colors.speaker1, "left")
-        self.buffer = ""
     end
 
     self.chat:update(dt)
-
     self.scenario:update(dt)
 end
 
 function Play:draw()
-    local threshold = input.beep:isDown() and dahThreshold or letterThreshold
-
-    if self.duration > threshold then
-        love.graphics.setColor(r.colors.indicatorDah)
-    else
-        love.graphics.setColor(r.colors.indicatorDit)
-    end
-
-    local lineWidth = u.clamp(self.duration / (threshold * 2) * w, 0, w)
-    love.graphics.rectangle("fill", 0, h - lineHeight, lineWidth, h)
-
-    love.graphics.setColor(r.colors.indicatorDit)
-    love.graphics.line(w / 2, h - lineHeight, w / 2, h)
-
-    love.graphics.setColor(r.colors.text)
-    love.graphics.print(self.buffer)
-
     self.chat:draw()
 end
 
---
+++ readers.lua
@@ -0,0 +1,80 @@
+local input = require "input"
+local morse = require "morse"
+
+local class = require "libs.middleclass"
+
+local MorseReader = class("MorseReader")
+
+function MorseReader:initialize(dahThreshold, letterThreshold)
+    self.dahThreshold = dahThreshold
+    self.letterThreshold = letterThreshold
+
+    self:reset()
+end
+
+function MorseReader:update(dt)
+    self.duration = self.duration + dt
+
+    if input.beep:pressed() then
+        self.duration = 0
+
+    elseif input.beep:released() then
+        self.code = self.code .. (self.duration > self.dahThreshold and "-" or ".")
+        self.duration = 0
+    end
+
+    local letter
+
+    if not input.beep:isDown() and self.code ~= "" and self.duration > self.letterThreshold then
+        letter = morse[self.code]
+        self.buffer = self.buffer .. letter
+        self.code = ""
+    end
+
+    return letter, self.buffer
+end
+
+function MorseReader:reset()
+    self.duration = 0
+    self.code = ""
+    self.buffer = ""
+end
+
+local MultiReader = class("MultiReader")
+
+function MultiReader:initialize(multiThreshold)
+    self.multiThreshold = multiThreshold
+
+    self:reset()
+end
+
+function MultiReader:update(dt)
+    self.duration = self.duration + dt
+
+    if input.beep:pressed() then
+        self.beeps = self.beeps + 1
+        self.duration = 0
+
+    elseif input.beep:released() then
+        self.duration = 0
+    end
+
+    if self.duration > self.multiThreshold and self.beeps > 0 then
+        local beeps = self.beeps
+        self.beeps = 0
+
+        if not input.beep:isDown() then
+            return beeps
+        end
+    end
+end
+
+function MultiReader:reset()
+    self.duration = 0
+    self.beeps = 0
+end
+
+return {
+    MorseReader = MorseReader,
+    MultiReader = MultiReader
+}

b658fc3e514bde4b075c385c111770741e2684de
pevzi <pevzi23@gmail.com>
2016-03-02 15:53:02 +0700

    Move coroutine-related stuff from scenario.lua to flow.lua

--- scenario.lua
+++ scenario.lua
@@ -1,6 +1,6 @@
 local class = require "libs.middleclass"
-local Stateful = require "libs.stateful"
 
+local Flow = require "flow"
 local r = require "resources"
 local input = require "input"
 
@@ -8,7 +8,7 @@ local utf8 = require "utf8"
 
 local initialState = "Intro"
 
-local Scenario = class("Scenario"):include(Stateful)
+local Scenario = class("Scenario", Flow)
 
 ------------------------------------------
 
@@ -476,30 +476,9 @@ local achievementTitles = {
 
 ------------------------------------------
 
-local function resume(co, ...)
-    local ok, msg = coroutine.resume(co, ...)
-
-    if not ok then
-        error(msg)
-    end
-end
-
-local Timer = class("Timer")
-
-function Timer:initialize(t)
-    self.t = t or 0
-end
-
-function Timer:set(t)
-    self.t = t
-end
-
-function Timer:update(dt)
-    self.t = self.t - dt
-    return self.t <= 0
-end
-
 function Scenario:initialize(game)
+    Scenario.super.initialize(self)
+
     self.game = game
 
     self.game.chat:registerSpeaker(1, r.colors.speaker1, "left")
@@ -507,36 +486,7 @@ function Scenario:initialize(game)
 
     self.game.achievements:setList(achievementList, achievementTitles)
 
-    self.timer = Timer()
-
     self:runState(initialState)
-end
-
-function Scenario:runState(stateName, ...)
-    self:gotoState(stateName, ...)
-
-    if self.act then
-        if self.co and coroutine.running() == self.co then
-            self:act(...)
-        else
-            self.co = coroutine.create(self.act)
-            resume(self.co, self, ...)
-        end
-    end
-end
-
-function Scenario:wait(worker)
-    self.worker = worker
-    coroutine.yield()
-end
-
-function Scenario:sleep(duration)
-    if duration then
-        self.timer:set(duration)
-        self:wait(self.timer)
-    else
-        self:wait()
-    end
 end
 
 function Scenario:say(id, text, delay)
@@ -545,17 +495,6 @@ function Scenario:say(id, text, delay)
     end
 
     self.game.chat:say(id, text)
-end
-
-function Scenario:update(dt)
-    if self.worker and self.worker:update(dt) then
-        self.worker = nil
-        resume(self.co)
-    end
-
-    if self.listen then
-        self:listen(dt)
-    end
 end
 
 return Scenario
